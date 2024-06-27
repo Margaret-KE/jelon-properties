0de3cb2fc2b6ab90cbd9019976fb59be3e02a6d3
petermeme <petermeme90@gmail.com>
2024-06-27 20:52:25 +0300

    athentication commit

--- fintrack/core/urls.py
+++ fintrack/core/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name = "home"),
+	path('user_login/', views.user_login, name='user_login'),
+	path('user_signup/', views.user_signup, name='user_signup'),
 	path('property/', views.property, name = "property"),
+	    path('logout/', views.user_logout, name='user_logout'),
 	path('property_single', views.property_single, name = 'property_single'),
 	path('contact', views.contact, name='contact'),
 	path('blog_grid', views.blog_grid, name='blog_grid'),
